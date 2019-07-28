63de49d9fafa002b72e044918a2b21371f55bb0c
johncho-cb <john.cho@cerebras.net>
2019-07-28 19:26:47 +0000

    More tests

---
+++ mnist/slurm_resolver_test_3_nodes.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+from cb_slurm_cluster_resolver import CbSlurmClusterResolver
+
+if __name__ == '__main__':
+    slurm_cluster_resolver = CbSlurmClusterResolver(
+        jobs={
+            'chief': 1,
+            'worker': 2,
+            'evaluator': 0,
+        },
+        port_base=8888)
+
+    cluster_spec = slurm_cluster_resolver.cluster_spec()
+    print(cluster_spec)
+    print(slurm_cluster_resolver.get_task_info())
+    print(slurm_cluster_resolver.master())
--- mnist/slurm_resolver_test_3_nodes_2_tasks.py
+++ mnist/slurm_resolver_test_3_nodes_2_tasks.py
@@ -5,10 +5,12 @@ if __name__ == '__main__':
     slurm_cluster_resolver = CbSlurmClusterResolver(
         jobs={
             'chief': 1,
-            'worker': 2,
-            'evaluator': 0,
+            'worker': 4,
+            'evaluator': 1,
         },
         port_base=8888)
 
     cluster_spec = slurm_cluster_resolver.cluster_spec()
     print(cluster_spec)
+    print(slurm_cluster_resolver.get_task_info())
+    print(slurm_cluster_resolver.master())
